bd71cac42217f4c9d60debce8f14505d93967fd2
jiangsong <jiangsong@bbdservice.com>
2018-07-30 11:08:14 +0800

    fix some bug

--- boarpc/constants.py
+++ boarpc/constants.py
@@ -3,8 +3,9 @@
 from common.config.conf import config
 class Constants:
     SERVICE_KEY=''
+    PROJECT_NAME = config.getConf('app', 'project_name', '')
     APP_MODULE_NAME = config.getConf('app','app_name','')
-    APP_REGISTER_ROOT_PATH = '/boa_rpc/'+config.getConf('app','app_name','')
-    APP_REGISTER_PROVIDER_PATH = '/boa_rpc/%s/%s' % (config.getConf('app', 'app_name', ''),'providers')
-    APP_REGISTER_CONSUMER_PATH = '/boa_rpc/%s/%s' % (config.getConf('app', 'app_name', ''), 'consumers')
+    APP_REGISTER_ROOT_PATH = '/%s_boa_rpc/%s' % (PROJECT_NAME,APP_MODULE_NAME)
+    APP_REGISTER_PROVIDER_PATH = '/%s_boa_rpc/%s/%s' % (PROJECT_NAME,APP_MODULE_NAME,'providers')
+    APP_REGISTER_CONSUMER_PATH = '/%s_boa_rpc/%s/%s' % (PROJECT_NAME,APP_MODULE_NAME, 'consumers')
     APP_REGISTER_PROVIDER_CHILD_PATH= APP_REGISTER_PROVIDER_PATH + '/{0}'
