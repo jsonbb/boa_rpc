3ca32385c5f0c595b010196456da0590a02b5a5e
jiangsong <jiangsong@bbdservice.com>
2018-09-13 08:53:18 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,6 @@
+##所需Python依赖
+-  thrift==0.11.0
+-  gevent==1.3.1
+-  kazoo==2.4.0
+-  gunicorn==19.3.0
+-  demjson==2.2.4
--- tgunicorn/thrift/six.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-DEFAULT_WORKER = "thrift_sync"
-DEFAULT_TRANSPORT = "thrift.transport.TTransport:TBufferedTransportFactory"
-DEFAULT_PROTOCOL = "thrift.protocol.TBinaryProtocol:TBinaryProtocolAcceleratedFactory"
-AVAILABLE_WORKERS = {
-    'thrift_sync': 'tgunicorn.thrift.sync_worker.SyncThriftWorker',
-    'thrift_gevent': 'tgunicorn.thrift.gevent_worker.GeventThriftWorker',
-    }
